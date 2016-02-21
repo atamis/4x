635390014a385fd59b7955a9df8522b81e4d19d8
Andrew Amis <atamiser@gmail.com>
2016-02-21 12:34:30 -0800

    Added Hex and WorldMap.
    No real functionality change, but game objects aren't continually
    destroyed and remade, and the rest of the game has a good structure to
    build on.

--- Assets/src/GameManager.cs
+++ Assets/src/GameManager.cs
@@ -6,34 +6,24 @@ namespace game {
     public class GameManager : MonoBehaviour {
 
         PlayerCamera pc;
-        private HashSet<HexLoc> hexes;
         private Layout l;
+        private WorldMap w;
 
         // Use this for initialization
         void Start() {
 
+            this.l = new Layout(Orientation.Pointy, new Vector2(1, 1), new Vector2(0, 0));
+
+
+            w = new GameObject("World Map").AddComponent<WorldMap>();
+            w.init(l);
+
             pc = new GameObject("Player Camera").AddComponent<PlayerCamera>();
             pc.init(Camera.main);
 
+            
 
-            hexes = new HashSet<HexLoc>();
-            this.l = new Layout(Orientation.Pointy, new Vector2(1, 1), new Vector2(0, 0));
-
-            HexLoc l = new HexLoc(0, 0, 0);
-            print(l);
-            print((l + HexLoc.Direction(0)));
-            print(l.Neighbor(0));
-
-            for (int i = 0; i < 10; i++) {
-                print(l.Neighbor(i));
-            }
-
-
-            for (int q = 0; q <= 10; q++) {
-                for (int r = 0; r <= 10; r++) {
-                    hexes.Add(new HexLoc(q, r, -q - r));
-                }
-            }
+            
         }
             
 
@@ -42,19 +32,7 @@ namespace game {
 
         // Update is called once per frame
         void Update() {
-            foreach (Transform children in transform) {
-                Destroy(children.gameObject);
-            }
-
-
-            foreach (HexLoc h in hexes) {
-                GameObject o = new GameObject(h.ToString());
-                o.transform.parent = gameObject.transform;
-                o.transform.localPosition = l.HexPixel(h);
-
-                var sp = o.AddComponent<SpriteRenderer>();
-                sp.sprite = Resources.Load<Sprite>("Textures/Hexagon");
-            }
+            
         }
     } 
 }
--- Assets/src/map/Hex.cs
+++ Assets/src/map/Hex.cs
@@ -2,75 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace game.map {
-    struct HexLoc {
-        static HexLoc[] hex_directions = {
-            new HexLoc(1, 0, -1), new HexLoc(1, -1, 0), new HexLoc(0, -1, 1),
-            new HexLoc(-1, 0, 1), new HexLoc(-1, 1, 0), new HexLoc(0, 1, -1)
-        };
+    class Hex : MonoBehaviour {
+        WorldMap w;
+        public HexLoc loc;
 
+        HexModel model;
 
-        public int q, r, s;
+        public void init(WorldMap w, HexLoc loc) {
+            this.w = w;
+            this.loc = loc;
 
-        public HexLoc(int q, int r, int s) {
-            this.q = q;
-            this.r = r;
-            this.s = s;
+            transform.localPosition = w.l.HexPixel(loc);
 
-            if (q + r + s != 0) {
-                throw new Exception("Invalid Hex Coordinates");
+            var o = new GameObject("Hex Model");
+            o.transform.parent = transform;
+            o.transform.localPosition = new Vector2(0, 0);
+            model = o.AddComponent<HexModel>();
+            model.init(this);
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+        private class HexModel : MonoBehaviour {
+            public Hex h;
+            SpriteRenderer sp;
+
+            public void init(Hex h) {
+                this.h = h;
+
+                sp = gameObject.AddComponent<SpriteRenderer>();
+
+                sp.sprite = Resources.Load<Sprite>("Textures/Hexagon");
             }
-        }
-        
-        public override bool Equals(Object o) {
-            if (o.GetType() != typeof(HexLoc)) {
-                return false;
-            }
-
-            HexLoc h = (HexLoc) o;
-
-            return this == h;
-        }
-
-        public override int GetHashCode() {
-            return q ^ r ^ s;
-        }
-
-        public static bool operator ==(HexLoc a, HexLoc b) {
-            return a.q == b.q && a.r == b.r && a.s == b.s;
-        }
-
-        public static bool operator !=(HexLoc a, HexLoc b) {
-            return !(a == b);
-        }
-
-        public static HexLoc operator +(HexLoc a, HexLoc b) {
-            return new HexLoc(a.q + b.q, a.r + b.r, a.s + b.s);
-        }
-
-        public static HexLoc operator -(HexLoc a, HexLoc b) {
-            return new HexLoc(a.q - b.q, a.r - b.r, a.s - b.s);
-        }
-
-        public int Length() {
-            return (int) (Math.Abs(q) + Math.Abs(r) + Math.Abs(s)) / 2;
-        }
-
-        public int Distance(HexLoc b) {
-            return (this - b).Length();
-        }
-
-        public static HexLoc Direction(int direction) {
-            return hex_directions[direction % 6];
-        }
-
-        public HexLoc Neighbor(int direction) {
-            return this + Direction(direction);
-        }
-
-        public override String ToString() {
-            return String.Format("[hexloc {0}, {1}, {2}]", q, r, s);
         }
     }
 }
