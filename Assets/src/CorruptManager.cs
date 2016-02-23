b02a4669f8b87a525294562b03093a9c25e3cb0a
Alacar <rtomcik@oberlin.edu>
2016-02-23 13:07:41 -0500

    Added a Corruption Manager

---
+++ Assets/src/CorruptManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Handles the behavior for the Corruption
+namespace game {
+	public class CorruptManager : MonoBehaviour {
+
+		GameManager parent;
+
+		// Use this for initialization
+		public void init (GameManager parent) {
+			this.parent = parent;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
