5f132f238fb1a6e6e461e2abeb9e5a4ef7455c77
Andrew Amis <atamiser@gmail.com>
2016-02-23 10:51:39 -0800

    Added basic turn system.
    Added actors, commands they can issue, and a list of actors to cycle
    through on end turn commands. Only 1 command though: end turn.

---
+++ Assets/src/input/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using game.actor;
+
+namespace game.input {
+    class PlayerControl : MonoBehaviour {
+        public Player p;
+
+        public void init(Player p) {
+            this.p = p;
+        }
+
+        void Start() {
+            
+        }
+
+        void Update() {
+
+        }
+
+        void OnGUI() {
+            if (GUI.Button(new Rect(10, 10, 100, 20), "End Turn")) {
+                p.AddCommand(new EndTurnCommand(p));
+            }
+        }
+
+    }
+}
