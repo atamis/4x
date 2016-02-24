387815e990a21add6c8ee72c0e9bea350bb84435
Andrew Amis <atamiser@gmail.com>
2016-02-23 17:26:51 -0800

    Move command now takes into account movement
    You can only move so far, but the move command doesn't take into account
    passability/impassability.

--- Assets/src/actor/MoveCommand.cs
+++ Assets/src/actor/MoveCommand.cs
@@ -7,19 +7,27 @@ using game.map;
 
 namespace game.actor {
     class MoveCommand : Command {
+        // TODO: doesn't take passability/impassability into account. Doesn't do any routing.
         Unit u;
         Hex target;
         Hex oldLocation;
+        int distance;
 
         public MoveCommand(Actor a, Unit u, Hex target) : base(a) {
             this.u = u;
             this.oldLocation = u.h;
             this.target = target;
+            distance = target.loc.Distance(oldLocation.loc);
+            if (distance > u.movement) {
+                throw new Exception(u + " doesn't have enough movement points (" +
+                    u.movement + " vs. " + target.loc.Distance(oldLocation.loc)  + ")");
+            }
         }
 
         public override void Apply(WorldMap w) {
             Console.WriteLine("Moved to " + target);
             u.h = target;
+            u.movement -= distance;
         }
 
         public override void Undo(WorldMap w) {
