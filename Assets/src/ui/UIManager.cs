2212ba4b536388c7ad1fa1bc8910fe9e1dd00d4e
Andrew Amis <atamiser@gmail.com>
2016-03-14 16:29:22 -0700

    Added keyboard commands to the build menu.
    Also cleaned up the building UI code.

--- Assets/src/ui/UIManager.cs
+++ Assets/src/ui/UIManager.cs
@@ -149,11 +149,35 @@ namespace game.ui {
                                 p.AddAllCommands(MoveCommand.pathfind(w, p, u, h));
                                 this.h_target = h;
                             } catch (Exception e) {
-                                EventManager.PostInvalidAction (new InvalidActionArgs{ msg = e.Message });
+                                EventManager.PostInvalidAction(new InvalidActionArgs { msg = e.Message });
                             }
                         }
 
                     }
+                }
+            }
+
+            if (state == State.Selected &&
+                h_target.unit != null && 
+                Input.GetKeyUp(KeyCode.B)) {
+                state = State.Building;
+            }
+
+            if (state == State.Building) {
+                if (Input.GetKeyUp(KeyCode.Alpha1)) {
+                    buildBuilding(BuildingType.Conduit);
+                }
+
+                if (Input.GetKeyUp(KeyCode.Alpha2)) {
+                    buildBuilding(BuildingType.Harvester);
+                }
+
+                if (Input.GetKeyUp(KeyCode.Alpha3)) {
+                    buildBuilding(BuildingType.Purifier);
+                }
+
+                if (Input.GetKeyUp(KeyCode.Alpha4)) {
+                    buildBuilding(BuildingType.WarpGate);
                 }
             }
         }
@@ -171,6 +195,16 @@ namespace game.ui {
 		private Hex getSelected() {
 			return h_target;
 		}
+
+        private void buildBuilding(BuildingType type) {
+            try {
+                p.AddCommand(new BuildCommand(p, u_target, h_target, type));
+            } catch (Exception e) {
+                print(e);
+            }
+            EventManager.TriggerBuildEvent(new BuildEventArgs { name = type.ToString(), turns = type.BuildTotal() / type.BuildPerTurn() });
+            state = State.Selected;
+        }
 
 		void OnGUI() {
 			GUILayout.BeginArea(new Rect (Screen.width*.3f, Screen.height*.8f, Screen.width/2, Screen.height*.9f));
@@ -252,47 +286,23 @@ namespace game.ui {
 
 				ButtonStyle.normal.background = UI_Cond; ButtonStyle.hover.background = UI_CondH; ButtonStyle.active.background = UI_CondC;
 				if (GUILayout.Button ("", ButtonStyle, GUILayout.Width (Screen.height * 0.08f), GUILayout.Height (Screen.height * 0.08f))) {
-					try {
-						p.AddCommand(new BuildCommand(p, u_target, h_target, BuildingType.Conduit));
-					} catch (Exception e) {
-						print (e);
-					}
-					EventManager.TriggerBuildEvent(new BuildEventArgs{name = "Conduit", turns = BuildingType.Conduit.BuildTotal()/BuildingType.Conduit.BuildPerTurn()});
-					state = State.Selected;
-				}
+                    buildBuilding(BuildingType.Conduit);
+                }
 
 				ButtonStyle.normal.background = UI_Harv; ButtonStyle.hover.background = UI_HarvH; ButtonStyle.active.background = UI_HarvC;
 				if (GUILayout.Button ("", ButtonStyle, GUILayout.Width (Screen.height * 0.08f), GUILayout.Height (Screen.height * 0.08f))) {
-					try {
-						p.AddCommand(new BuildCommand(p, u_target, h_target, BuildingType.Harvester));
-					} catch (Exception e) {
-						print (e);
-					}
-					EventManager.TriggerBuildEvent(new BuildEventArgs{name = "Harvester", turns = BuildingType.Harvester.BuildTotal()/BuildingType.Harvester.BuildPerTurn()});
-					state = State.Selected;
-				}
+                    buildBuilding(BuildingType.Harvester);
+                }
 
 				ButtonStyle.normal.background = UI_Tow; ButtonStyle.hover.background = UI_TowH; ButtonStyle.active.background = UI_TowC;
 				if (GUILayout.Button ("", ButtonStyle, GUILayout.Width (Screen.height * 0.08f), GUILayout.Height (Screen.height * 0.08f))) {
-					try {
-						p.AddCommand(new BuildCommand(p, u_target, h_target, BuildingType.Purifier));
-					} catch (Exception e) {
-						print (e);
-					}
-					EventManager.TriggerBuildEvent(new BuildEventArgs{name = "Purifier", turns = BuildingType.Purifier.BuildTotal()/BuildingType.Purifier.BuildPerTurn()});
-					state = State.Selected;
-				}
+                    buildBuilding(BuildingType.Purifier);
+                }
 
 				ButtonStyle.normal.background = UI_Gate; ButtonStyle.hover.background = UI_GateH; ButtonStyle.active.background = UI_GateC;
 				if (GUILayout.Button ("", ButtonStyle, GUILayout.Width (Screen.height * 0.08f), GUILayout.Height (Screen.height * 0.08f))) {
-					try {
-						p.AddCommand(new BuildCommand(p, u_target, h_target, BuildingType.WarpGate));
-					} catch (Exception e) {
-						print (e);
-					}
-					EventManager.TriggerBuildEvent(new BuildEventArgs{name = "Warp Gate", turns = BuildingType.WarpGate.BuildTotal()/BuildingType.WarpGate.BuildPerTurn()});
-					state = State.Selected;
-				}
+                    buildBuilding(BuildingType.WarpGate);
+                }
 				GUILayout.EndHorizontal ();
 				GUILayout.EndArea ();
 			}
